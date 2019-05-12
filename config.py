8e18ed6a764731ae4bc5d836ab759d1461c8d6f6
Ceisler <ceisler@4ilmceisler.ad.here.com>
2019-05-12 07:09:10 +0300

    first commit

---
+++ config.py
@@ -0,0 +1,12 @@
+#config file
+url = 'https://www.alljobs.co.il/Search/UploadSingle.aspx?JobID='
+title = 'support'
+commonWords = ['a','and','as','company','expirience','for','in','is','management','of','or','such','the','to','with','also','an','but','desire','to','make','experience','tools','years','engineer','skills','development','technologies','at','software','looking','our','least','on','undestanding','are','required','working','we','offices','etc','that','field','like','build','big','other','new','you','all','will','one','improve','knowlege','using']
+# fromID = '5333119' 
+# ToID = '5333122'
+
+
+fromID = '5320456' 
+ToID = '5339460'
+elasticURL = 'http://localhost:9200/%s/_doc/' % title
+#lastest 5339460
