f9a38c0684f36a8cc7a4fd7d56658a57049aa34d
Ceisler <ceisler@4ilmceisler.ad.here.com>
2019-08-19 15:27:41 +0300

    added title

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 #config file
 url = 'https://www.alljobs.co.il/Search/UploadSingle.aspx?JobID='
-title = 'support'
+title = 'devops'
 commonWords = ['a','and','as','company','expirience','for','in','is','management','of','or','such','the','to','with','also','an','but','desire','to','make','experience','tools','years','engineer','skills','development','technologies','at','software','looking','our','least','on','undestanding','are','required','working','we','offices','etc','that','field','like','build','big','other','new','you','all','will','one','improve','knowlege','using']
 # fromID = '5333119' 
 # ToID = '5333122'
